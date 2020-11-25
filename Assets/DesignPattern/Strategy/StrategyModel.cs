c57c30e0e0e604a0c44ebd397b05b513a90d4bc8
葡萄皮 <609612002@qq.com>
2020-11-25 17:28:07 +0800

    新增策略模式

---
+++ Assets/DesignPattern/Strategy/StrategyModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Strategy;
+
+namespace Strategy
+{
+    public class StrategyModel
+    {
+    }
+    public class Context
+    {
+        Strategy m_Strategy =null;
+        public void SetStrategy(Strategy strategy)
+        {
+            m_Strategy = strategy;
+        }
+        public void ActionStrategy()
+        {
+           if( m_Strategy==null)
+           {
+                Debug.LogError("m_Strategy is null!");
+                return;
+            }
+            m_Strategy.AlgorithmInterface();
+
+        }
+    }
+    public abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+    public class StrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Debug.Log("算法A");
+        }
+    }
+    public class StrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+         
+        {
+            Debug.Log("算法B");
+        }
+    }
+    public class StrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Debug.Log("算法C");
+        }
+    }
+    public interface Test
+    {
+         StrategyC StrategyC { get; set; }
+        
+    }
+      public class a
+    {
+        string ac;
+        int b;
+    }
+
+}
+
+
