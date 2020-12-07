6b508afa6a516e70501159510d7e992fe8209b26
葡萄皮 <609612002@qq.com>
2020-12-07 17:18:54 +0800

    新增访问者和装饰者模式

---
+++ Assets/DesignPattern/Decorator/DecoratorModel.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DocoratorModelTest
+{
+    public class DecoratorModel : MonoBehaviour
+    {
+        private void Start()
+        {
+            HeadEquipment headEquipment = new HeadEquipment();
+            headEquipment.EquipmentName = "头盔";
+            BorderDecorator border = new BorderDecorator(headEquipment);
+            border.ShowInfo();
+        }
+    }
+    public abstract class Equipment
+    {
+        public  string EquipmentName { get; set; }
+        public abstract void ShowInfo();
+
+        public virtual string  GetEquipmentName()
+        {
+            return EquipmentName;
+        }
+
+    }
+    public class HeadEquipment : Equipment
+    {
+        public override void ShowInfo()
+        {
+            Debug.Log("装备名字:" + EquipmentName);
+        }
+    }
+    public abstract class Decorator : Equipment
+    {
+        Equipment equipment;
+        public Decorator(Equipment equipment)
+        {
+            this.equipment = equipment;
+        }
+
+        public override void ShowInfo()
+        {
+            equipment.ShowInfo();
+        }
+        public override string GetEquipmentName()
+        {
+           return equipment.GetEquipmentName();
+        }
+    }
+    public class BorderDecorator : Decorator
+    {
+        Border border = new Border();
+        public BorderDecorator(Equipment equipment) : base(equipment) { }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            border.ShowInfo(this);
+        }
+    }
+    public abstract class Additional
+    {
+        public abstract void ShowInfo(Equipment equipment);
+
+    }
+    public class Border : Additional
+    {
+        public override void ShowInfo(Equipment equipment)
+        {
+            Debug.Log("Border" + equipment.GetEquipmentName());
+        }
+    }
+
+}
+
---
+++ Assets/DesignPattern/Visitor/VisitorModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisitorModelTest
+{
+    public class VisitorModel : MonoBehaviour
+    {
+        private void Start()
+        {
+            ShapeContainer shapeContainer = new ShapeContainer();
+            shapeContainer.AddShape(new Sphere());
+            shapeContainer.RunVisitor(new DrawVisitor());
+        }
+    }
+    public abstract class ShapeVisitor
+    {
+        public virtual void VisitSphere(Sphere sphere)
+        {
+        }
+        public virtual void VisitCube()
+        {
+
+        }
+        public virtual void VisitCylinder()
+        {
+
+        }
+    }
+    public class ShapeContainer
+    {
+        List<Shape> m_Shapes = new List<Shape>();
+        public void AddShape(Shape shape)
+        {
+            m_Shapes.Add(shape);
+        }
+        public void RunVisitor(ShapeVisitor shapeVisitor)
+        {
+            foreach (var item in m_Shapes)
+            {
+                item.RunVisitor(shapeVisitor);
+            }
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract void Draw();
+        public abstract void RunVisitor(ShapeVisitor shapeVisitor);
+    }
+    public class Sphere:Shape
+    {
+        public override void Draw()
+        {
+            Debug.Log("SphereDraw");
+        }
+
+        public override void RunVisitor(ShapeVisitor shapeVisitor)
+        {
+            shapeVisitor.VisitSphere(this);
+        }
+    }
+    public class DrawVisitor:ShapeVisitor
+    {
+        public override void VisitSphere(Sphere sphere)
+        {
+            sphere.Draw();
+        }
+    }
+}
+
